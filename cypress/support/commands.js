5829ece1613bc6b26d467909521917c8cb564511
Marcia Marrocos Amaral <125309272+marcinhanota1000@users.noreply.github.com>
2026-09-19 23:55:47 -0300

    Melhora os comandos customizados do Cypress para maior estabilidade e clareza

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,21 +1,19 @@
-
 Cypress.Commands.add('login', (usuario, senha) => {
-    cy.visit('')
-    cy.get('[data-test="username"]').type(usuario)
-    cy.get('[data-test="password"]').type(senha)
-    cy.get('[data-test="login-button"]').click()
-  })
+  cy.visit('/');
+  cy.get('[data-test="username"]').should('be.visible').clear().type(usuario);
+  cy.get('[data-test="password"]').should('be.visible').clear().type(senha);
+  cy.get('[data-test="login-button"]').click();
+});
 
-  Cypress.Commands.add('cadastro', (nome, sobrenome, cep) => {
-    cy.get('[data-test="firstName"]').type(nome)
-    cy.get('[data-test="lastName"]').type(sobrenome)
-    cy.get('[data-test="postalCode"]').type(cep)
-    cy.get('[data-test="continue"]').click()
-  })
+Cypress.Commands.add('cadastro', (nome, sobrenome, cep) => {
+  cy.get('[data-test="firstName"]').should('be.visible').clear().type(nome);
+  cy.get('[data-test="lastName"]').should('be.visible').clear().type(sobrenome);
+  cy.get('[data-test="postalCode"]').should('be.visible').clear().type(cep);
+  cy.get('[data-test="continue"]').click();
+});
 
-  Cypress.Commands.add('escolherProduto', (nomeProduto) => {
-    cy.contains(nomeProduto).click()
-    cy.get('[data-test="add-to-cart"]').click()
-    cy.get('[data-test="back-to-products"]').click()
-  })
-
+Cypress.Commands.add('escolherProduto', (nomeProduto) => {
+  cy.contains('[data-test="inventory-item-name"]', nomeProduto).click();
+  cy.get('[data-test="add-to-cart"]').click();
+  cy.get('[data-test="back-to-products"]').click();
+});
