8cd7f3db273a7d39e538d5206e46dfc2147eabe5
Marcia Marrocos Amaral <125309272+marcinhanota1000@users.noreply.github.com>
2026-09-19 23:56:06 -0300

    Ajusta a configuração do Cypress para execução correta dos testes E2E

--- cypress.config.js
+++ cypress.config.js
@@ -5,8 +5,11 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
     chromeWebSecurity: false,
-    baseUrl: 'https://www.saucedemo.com/',
-    video: true
+    baseUrl: "https://www.saucedemo.com/",
+    video: true,
+    screenshotOnRunFailure: true,
+    defaultCommandTimeout: 10000,
   },
 });
